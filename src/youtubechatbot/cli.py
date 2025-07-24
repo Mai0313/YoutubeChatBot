eabdddd1dd58c14f130a815a65fe61b33c1c7f10
wei <weichenglee0313@gmail.com>
2025-07-24 22:26:26 +0800

    feat: simplify YouTube API interactions and enhance chat message handling

--- src/youtubechatbot/cli.py
+++ src/youtubechatbot/cli.py
@@ -1,16 +1,13 @@
 import time
-from typing import TYPE_CHECKING, Any
+from typing import Any
 from urllib.parse import urlparse
 
 import dotenv
 from pydantic import Field, computed_field
 from rich.console import Console
 from pydantic_settings import BaseSettings
-from googleapiclient.discovery import Resource, build
-
-if TYPE_CHECKING:
-    from googleapiclient.http import HttpRequest
-
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 
 dotenv.load_dotenv()
 console = Console()
@@ -26,32 +23,27 @@ class YoutubeStream(Config):
 
     @computed_field
     @property
-    def youtube(self) -> Resource:
-        youtube: Resource = build(
-            serviceName="youtube", version="v3", developerKey=self.youtube_data_api_key
-        )
-        return youtube
-
-    @computed_field
-    @property
     def video_id(self) -> str:
         parsed_url = urlparse(url=self.url)
         video_id = parsed_url.query.split("=")[-1]
         return video_id
 
     def get_chat_id(self) -> str:
-        video_info: HttpRequest = self.youtube.videos().list(
-            part="liveStreamingDetails", id=self.video_id
+        youtube = build(
+            serviceName="youtube", version="v3", developerKey=self.youtube_data_api_key
         )
-        response = video_info.execute()
+        response = youtube.videos().list(part="liveStreamingDetails", id=self.video_id).execute()
         chat_id: str = response["items"][0]["liveStreamingDetails"]["activeLiveChatId"]
         return chat_id
 
     def get_chat_messages(self) -> None:
         live_chat_id = self.get_chat_id()
+        youtube = build(
+            serviceName="youtube", version="v3", developerKey=self.youtube_data_api_key
+        )
         next_token = None
         while True:
-            live_messages: HttpRequest = self.youtube.liveChatMessages().list(
+            live_messages = youtube.liveChatMessages().list(
                 liveChatId=live_chat_id, part="snippet,authorDetails", pageToken=next_token
             )
             response: dict[str, dict[str, Any]] = live_messages.execute()
@@ -65,7 +57,26 @@ class YoutubeStream(Config):
             next_token = response.get("nextPageToken")
             time.sleep(2000)
 
-    def reply_to_chat(self, message: str) -> None: ...
+    def reply_to_chat(self, message: str) -> None:
+        live_chat_id = self.get_chat_id()
+        flow = InstalledAppFlow.from_client_secrets_file(
+            client_secrets_file="client_secret.json",
+            scopes=["https://www.googleapis.com/auth/youtube.force-ssl"],
+        )
+        credentials = flow.run_local_server(port=0)
+
+        youtube = build(serviceName="youtube", version="v3", credentials=credentials)
+
+        youtube.liveChatMessages().insert(
+            part="snippet",
+            body={
+                "snippet": {
+                    "liveChatId": live_chat_id,
+                    "type": "textMessageEvent",
+                    "textMessageDetails": {"messageText": message},
+                }
+            },
+        ).execute()
 
 
 def main() -> None:
